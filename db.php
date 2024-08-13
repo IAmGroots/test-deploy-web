bb5c8f9778ab56bf7a5406a21c70ca11b1c46069
Hadiseptian <hadiseptian.crb@gmail.com>
2024-08-13 16:00:54 +0800

    fix connection

--- db.php
+++ db.php
@@ -4,15 +4,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// $servername = $_ENV['DB_SERVERNAME'];
-// $username = $_ENV['DB_USERNAME'];
-// $password = $_ENV['DB_PASSWORD'];
-// $dbname = $_ENV['DB_NAME'];
+$servername = $_ENV['DB_SERVERNAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
 
-$servername = getenv('DB_SERVERNAME');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
-$dbname = getenv('DB_NAME');
+// $servername = getenv('DB_SERVERNAME');
+// $username = getenv('DB_USERNAME');
+// $password = getenv('DB_PASSWORD');
+// $dbname = getenv('DB_NAME');
 
 // Debug output
 // echo "Servername: " . $servername . "<br>";
@@ -27,6 +27,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
-echo "Koneksi berhasil!";
 ?>
