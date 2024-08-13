6635a23c71cf68fcd8cec97c99d060148e635ff7
Hadiseptian <86460497+IAmGroots@users.noreply.github.com>
2024-08-13 16:02:22 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,21 +4,21 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$servername = $_ENV['DB_SERVERNAME'];
-$username = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB_NAME'];
+// $servername = $_ENV['DB_SERVERNAME'];
+// $username = $_ENV['DB_USERNAME'];
+// $password = $_ENV['DB_PASSWORD'];
+// $dbname = $_ENV['DB_NAME'];
 
-// $servername = getenv('DB_SERVERNAME');
-// $username = getenv('DB_USERNAME');
-// $password = getenv('DB_PASSWORD');
-// $dbname = getenv('DB_NAME');
+$servername = getenv('DB_SERVERNAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 
 // Debug output
-// echo "Servername: " . $servername . "<br>";
-// echo "Username: " . $username . "<br>";
-// echo "Password: " . $password . "<br>";
-// echo "Database Name: " . $dbname . "<br>";
+echo "Servername: " . $servername . "<br>";
+echo "Username: " . $username . "<br>";
+echo "Password: " . $password . "<br>";
+echo "Database Name: " . $dbname . "<br>";
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
