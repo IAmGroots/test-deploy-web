34598d8feb196dbf7da68c65f116a31de9c5e8e8
Hadiseptian <86460497+IAmGroots@users.noreply.github.com>
2024-08-13 14:17:46 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,10 +4,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$servername = $_ENV['DB_SERVERNAME'];
-$username = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB_NAME'];
+// $servername = $_ENV['DB_SERVERNAME'];
+// $username = $_ENV['DB_USERNAME'];
+// $password = $_ENV['DB_PASSWORD'];
+// $dbname = $_ENV['DB_NAME'];
+
+$servername = getenv('DB_SERVERNAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 
 // Debug output
 // echo "Servername: " . $servername . "<br>";
