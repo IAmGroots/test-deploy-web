6eeafc8c658cce329a863fde0a5098b9c5e6dce7
Hadiseptian <86460497+IAmGroots@users.noreply.github.com>
2024-08-13 14:10:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,33 @@
 // header("Location: view/listUser.php");
 // exit();
 echo("Hello world");
+include_once '../db.php';
+function getUsers() {
+    global $conn;
+    $sql = "SELECT * FROM users";
+    return $conn->query($sql);
+}
+
+$users = getUsers();
 ?>
+
+<h2>Daftar Pengguna</h2>
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Aksi</th>
+    </tr>
+    <?php while($row = $users->fetch_assoc()) { ?>
+    <tr>
+        <td><?php echo $row['id']; ?></td>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['email']; ?></td>
+        <td>
+            <a href="editUser.php?id=<?php echo $row['id']; ?>">Edit</a>
+            <a href="deleteUser.php?id=<?php echo $row['id']; ?>">Delete</a>
+        </td>
+    </tr>
+    <?php } ?>
+</table>
