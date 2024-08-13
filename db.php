585bc4bccdd6488512be6550ff07361113000db4
Hadiseptian <hadiseptian.crb@gmail.com>
2024-08-13 12:55:43 +0800

    add config .env

--- db.php
+++ db.php
@@ -1,8 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_deploy";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_SERVERNAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+// Debug output
+// echo "Servername: " . $servername . "<br>";
+// echo "Username: " . $username . "<br>";
+// echo "Password: " . $password . "<br>";
+// echo "Database Name: " . $dbname . "<br>";
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,4 +22,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+
+echo "Koneksi berhasil!";
 ?>
